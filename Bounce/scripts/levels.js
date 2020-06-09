c69c4cd1b0abd9a865f0b5e28bde569d47d3466f
ONG Yan Kai <yankai.ong.2016@sis.smu.edu.sg>
2020-06-09 23:51:49 +0800

    Level initialization and tile randomization, code cleanup

---
+++ Bounce/scripts/levels.js
@@ -0,0 +1,50 @@
+module.exports = [
+    {
+        no_of_tiles: 8,
+        tile_positions: [
+            [3,1], [3,2], [4,1], [4,2], [5,1], [5,2], [6,1], [6,2]
+        ],
+        tile_patterns: [
+            {
+                name: "tile1",
+                direction: "left",
+                units: 1
+            },
+            {
+                name: "tile2",
+                direction: "left",
+                units: 1
+            },
+            {
+                name: "tile3",
+                direction: "left",
+                units: 1
+            },
+            {
+                name: "tile4",
+                direction: "left",
+                units: 1
+            },
+            {
+                name: "tile5",
+                direction: "right",
+                units: 1
+            },
+            {
+                name: "tile6",
+                direction: "right",
+                units: 1
+            },
+            {
+                name: "tile7",
+                direction: "right",
+                units: 1
+            },
+            {
+                name: "tile8",
+                direction: "right",
+                units: 1
+            },
+        ]
+    },
+]
