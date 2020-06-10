fe24bb9ba95d8cf5ecf76f9cc0b5ee9c9959bf52
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2020-06-10 21:26:28 +0800

    Clean up code, add animations

--- Bounce/scripts/levels.js
+++ Bounce/scripts/levels.js
@@ -1,50 +1,118 @@
+let lv_1_start_position = [2, 1]; // start_tile
+let lv_1_end_position = [7, 2]; // end_tile
+let lv_1_positions = [ // NO 0-INDEXING
+  [3, 1],
+  [3, 2],
+  [4, 1],
+  [4, 2],
+  [5, 1],
+  [5, 2],
+  [6, 1],
+  [6, 2]
+];
+let lv_1_directions = ["left", "left", "...", "left"]; // DIRECTIONS FOR tile1 to tileN MUST BE IN SEQUENCE
+
+/** 
 module.exports = [
-    {
-        no_of_tiles: 8,
-        tile_positions: [
-            [3,1], [3,2], [4,1], [4,2], [5,1], [5,2], [6,1], [6,2]
-        ],
-        tile_patterns: [
-            {
-                name: "tile1",
-                direction: "left",
-                units: 1
-            },
-            {
-                name: "tile2",
-                direction: "left",
-                units: 1
-            },
-            {
-                name: "tile3",
-                direction: "left",
-                units: 1
-            },
-            {
-                name: "tile4",
-                direction: "left",
-                units: 1
-            },
-            {
-                name: "tile5",
-                direction: "right",
-                units: 1
-            },
-            {
-                name: "tile6",
-                direction: "right",
-                units: 1
-            },
-            {
-                name: "tile7",
-                direction: "right",
-                units: 1
-            },
-            {
-                name: "tile8",
-                direction: "right",
-                units: 1
-            },
-        ]
-    },
+  generateLevel(...),
+  generateLevel(...)
 ]
+**/
+
+function generateLevel(startPos, endPos, positions, directions) {
+  let tilePatterns = [];
+
+  for (let i = 0; i < positions.length; i++) {
+    tilePatterns.append({
+      name: "tile" + (i + 1),
+      direction: directions[i + 1],
+      units: 1
+    });
+  }
+
+  return {
+    no_of_tiles: positions.length + 2,
+    start_tile: {
+      name: "start_tile",
+      direction: "right",
+      units: 1,
+      position: startPos
+    },
+    end_tile: {
+      name: "end_tile",
+      position: endPos
+    },
+    tile_positions: positions,
+    tile_patterns: tilePatterns
+    // start_position: start_pos,
+    // end_position: end_pos,
+  };
+}
+
+module.exports = [
+  {
+    no_of_tiles: 8,
+    tile_positions: [
+      [3, 1],
+      [3, 2],
+      [4, 1],
+      [4, 2],
+      [5, 1],
+      [5, 2],
+      [6, 1],
+      [6, 2]
+    ],
+    start_tile: {
+        name: "start_tile",
+        direction: "right",
+        units: 1,
+        position: [2,1]
+      },
+    end_tile: {
+        name: "end_tile",
+        position: [7,2]
+      },
+    tile_patterns: [
+      {
+        name: "tile1",
+        direction: "left",
+        units: 1
+      },
+      {
+        name: "tile2",
+        direction: "left",
+        units: 1
+      },
+      {
+        name: "tile3",
+        direction: "left",
+        units: 1
+      },
+      {
+        name: "tile4",
+        direction: "left",
+        units: 1
+      },
+      {
+        name: "tile5",
+        direction: "right",
+        units: 1
+      },
+      {
+        name: "tile6",
+        direction: "right",
+        units: 1
+      },
+      {
+        name: "tile7",
+        direction: "right",
+        units: 1
+      },
+      {
+        name: "tile8",
+        direction: "right",
+        units: 1
+      }
+    ]
+  }
+];
--- Bounce/scripts/script.js
+++ Bounce/scripts/script.js
@@ -1,78 +1,144 @@
 // Imports
 const Scene = require('Scene');
 const TouchGestures = require("TouchGestures");
-const Reactive = require("Reactive");
+// const Reactive = require("Reactive");
 const Animation = require("Animation");
 const Time = require("Time");
+const CameraInfo = require('CameraInfo');
 export const Diagnostics = require('Diagnostics');
 
-// Scene objects
-const tiles = Scene.root.find("tiles");
-
-
-
 // Tile dimensions
-const unitLength = 0.15
-const bottomLeftx = -0.463
-const bottomLefty = -0.8
-const bottomLeftz = -0.52
+const unitLength = 0.15; // x length and z length
+const topLeftx = -0.463;
+const topLefty = -0.8;
+const topLeftz = -0.52;
+
+/**
+Grid
+          |-------|
+          |_      |
+(x, y, z) |l|_____|
+**/
 
 // Level variables
 const levels = require("./levels");
-let current_level = 1
-let level = levels[current_level - 1]
-let no_of_tiles = level.no_of_tiles
-let tile_positions = level.tile_positions
-let tile_patterns = level.tile_patterns
+let current_level = 1;
+let level = levels[current_level - 1]; // lv 1 is index 0
+let no_of_tiles = level.no_of_tiles;
+let tile_positions = level.tile_positions;
+let tile_patterns = level.tile_patterns;
+let start_tile = level.start_tile;
+let end_tile = level.end_tile;
+let position_direction = {}
 
 // Gameflow variables
-let bomb_released = false
-let selection = null
+let ready = false;
+let selection = null; // store any selected tile (for swapping)
 
+// Place start and end tile
+Scene.root.findFirst(start_tile.name)
+    .then(tile => {
+        placeTile(tile, start_tile.position);
+    });
+
+Scene.root.findFirst(end_tile.name)
+    .then(tile => {
+        placeTile(tile, end_tile.position); // Includes chest
+    });
+
+// Place character on start tile
+Scene.root.findFirst("character")
+    .then(agent => {
+        let agentPosition = start_tile.position
+        let point = getMidPointFromIndex(agentPosition);
+        agent.transform.x = point[0];
+        agent.transform.y = topLefty + 0.15; // To check height
+        agent.transform.z = point[1];
+
+        Time.setInterval(() => {
+            if (CameraInfo.isRecordingVideo.pinLastValue()) {
+                agentPosition = animateAgent(agent, agentPosition);
+            }
+        }, 1000);
+
+        Diagnostics.log("Agent loaded");
+    })
 
 // Place each tile in a random position
-tile_patterns.forEach(tile => {
-    let randIndex = getRandomInt(tile_positions.length)
-    let location = tile_positions[randIndex]
-    tile_positions.splice(randIndex, 1)
-    let tileObj = tiles.child(tile.name)
-    tileObj.transform.x = getCoordinateXFromIndex(location[0])
-    tileObj.transform.z = getCoordinateZFromIndex(location[1])
+for (let i = 1; i <= no_of_tiles; i++) {
+    Scene.root.findFirst("tile" + i)
+        .then(tile => {
+            let randIndex = getRandomInt(tile_positions.length)
+            let position = tile_positions[randIndex]
+            tile_positions.splice(randIndex, 1)
 
-    // For each tile, prepare listener for tap event
-    TouchGestures.onTap(tileObj).subscribe(function() {
-        if (!bomb_released) {
-            if (selection === null) {
-                // if there is no active tile
-                selection = tileObj
-                animateTileSelect(tileObj, "active")
-            } else {
-                // if active tile is same as selection, de-select tile
-                if (tileObj === selection) {
-                    animateTileSelect(tileObj, "blur")
-                    selection = null
+            placeTile(tile, position)
+
+            // For each tile, prepare listener for tap event
+            TouchGestures.onTap(tile).subscribe(function () {
+                if (!ready) {
+                    if (selection === null) {
+                        // if there is no active tile
+                        selection = tile
+                        animateTileSelect(tile, "active")
+                    } else {
+                        // if active tile is same as selection, de-select tile
+                        if (tile === selection) {
+                            animateTileSelect(tile, "blur")
+                            selection = null
+                        }
+                        // swap tiles
+                        else {
+                            animateTileSwap(selection, tile)
+                            animateTileSelect(selection, "blur")
+                            selection = null
+                        }
+                    }
                 }
-                // swap tiles
-                else {
-                    animateTileSwap(selection, tileObj)
-                    animateTileSelect(selection, "blur")
-                    selection = null
-                }
-            }
-        }
-    })
-})
+            });
 
+        });
+}
 
+// TODO: Track position of player and the tile direction
+
+// TODO: Movement animation
+// while (ready && !arrivedAtEndTile(agent)) {
+//     animateCharacter(agent)
+// }
 
 // Helper functions
+function placeTile(tileObj, position) {
+    tileObj.transform.x = getCoordinateXFromIndex(position[0]);
+    tileObj.transform.z = getCoordinateZFromIndex(position[1]);
+
+    if (tileObj.name === "start_tile") {
+        position_direction[position] = start_tile.direction;
+    } else if (tileObj.name === "end_tile") {
+        position_direction[position] = end_tile.direction;
+    } else {
+        position_direction[position] = tile_patterns.filter((tile) => tile.name === tileObj.name)[0].direction;
+    }
+
+    if (Object.keys(position_direction).length === (tile_patterns.length + 2)) {
+        // All tiles placed
+        Diagnostics.log("All tiles loaded");
+    }
+}
+
+function getMidPointFromIndex(position) {
+    return [
+        getCoordinateXFromIndex(position[0]) - (unitLength / 2),
+        getCoordinateZFromIndex(position[1]) + (unitLength / 2)
+    ]
+}
 
 function getCoordinateXFromIndex(index) {
-    return bottomLeftx + (index * unitLength)
+    return topLeftx + (index * unitLength)
 }
 
 function getCoordinateZFromIndex(index) {
-    return bottomLeftz + (index * unitLength)
+    return topLeftz + (index * unitLength)
 }
 
 function getRandomInt(max) {
@@ -81,66 +147,76 @@ function getRandomInt(max) {
 
 
 // Animations
-
-function animateTileSelect(tile, animation) {
-    const tdpTileMove = {
-        durationMilliseconds: 100,
-        loopCount: 1,
-        mirror: false
-    };
-
-    const tdTileMove = Animation.timeDriver(tdpTileMove);
-
-    const active = Animation.animate(
-        tdTileMove,
-        Animation.samplers.linear(
-            tile.transform.y.pinLastValue(),
-            tile.transform.y.pinLastValue() + 0.02
-        )
-    );
-
-    const blur = Animation.animate(
-        tdTileMove,
-        Animation.samplers.linear(
-            tile.transform.y.pinLastValue(),
-            tile.transform.y.pinLastValue() - 0.02
-        )
-    );
-
-    tile.transform.y = animation === "active" ? active : blur
-    tdTileMove.start()
+function getTimeDriver(duration = 200, loopCount = 1, mirror = false) {
+    return Animation.timeDriver({
+        durationMilliseconds: duration,
+        loopCount: loopCount,
+        mirror: mirror
+    });
 }
 
+function animateTileSelect(tile, animation) {
+    const tdTileMove = getTimeDriver();
+
+    let y_value = tile.transform.y.pinLastValue();
+    y_value = animation === "active" ? y_value + 0.02 : y_value - 0.02;
+
+    tile.transform.y = Animation.animate(
+        tdTileMove,
+        Animation.samplers.linear(tile.transform.y.pinLastValue(), y_value)
+    );
+
+    tdTileMove.start();
+}
+
+const shiftx = (td, obj, destination) =>
+    Animation.animate(td, Animation.samplers.linear(obj.transform.x.pinLastValue(), destination));
+
+const shiftz = (td, obj, destination) =>
+    Animation.animate(td, Animation.samplers.linear(obj.transform.z.pinLastValue(), destination));
+
 function animateTileSwap(tile1, tile2) {
-    const tdpTileSwap = {
-        durationMilliseconds: 100,
-        loopCount: 1,
-        mirror: false
-    };
+    const tdTileSwap = getTimeDriver();
 
-    const tdTileSwap = Animation.timeDriver(tdpTileSwap);
+    let tile1x = tile1.transform.x.lastValue;
+    let tile1z = tile1.transform.z.lastValue;
+    tile1.transform.x = shiftx(tdTileSwap, tile1, tile2.transform.x.lastValue);
+    tile1.transform.z = shiftz(tdTileSwap, tile1, tile2.transform.z.lastValue);
+    tile2.transform.x = shiftx(tdTileSwap, tile2, tile1x);
+    tile2.transform.z = shiftz(tdTileSwap, tile2, tile1z);
+    tdTileSwap.start();
+}
 
-    const shiftx = (tile, destination) => Animation.animate(
-        tdTileSwap,
-        Animation.samplers.linear(
-            tile.transform.x.pinLastValue(),
-            destination
-        )
-    );
+function animateAgent(agent, agentPosition) {
+    let direction = position_direction[agentPosition];
+    Diagnostics.watch("Current Position", agentPosition.toString());
+    Diagnostics.watch("Direction", direction);
+    let destination_position = null;
 
-    const shiftz = (tile, destination) => Animation.animate(
-        tdTileSwap,
-        Animation.samplers.linear(
-            tile.transform.z.pinLastValue(),
-            destination
-        )
-    );
+    if (direction == "left") {
+        destination_position = [agentPosition[0] - 1, agentPosition[1]];
+    } else if (direction == "right") {
+        destination_position = [agentPosition[0] + 1, agentPosition[1]];
+    } else if (direction == "up") {
+        destination_position = [agentPosition[0], agentPosition[1] - 1];
+    } else if (direction == "down") {
+        destination_position = [agentPosition[0], agentPosition[1] + 1];
+    }
 
-    let tile1x = tile1.transform.x.lastValue
-    let tile1z = tile1.transform.z.lastValue
-    tile1.transform.x = shiftx(tile1, tile2.transform.x.lastValue)
-    tile1.transform.z = shiftz(tile1, tile2.transform.z.lastValue)
-    tile2.transform.x = shiftx(tile2, tile1x)
-    tile2.transform.z = shiftz(tile2, tile1z)
-    tdTileSwap.start()
+    Diagnostics.watch("Destination Position", destination_position.toString());
+
+    if (destination_position === null || position_direction[destination_position] === undefined) {
+        // Position to move toward is invalid
+        ready = false;
+        return;
+    }
+
+    const tdAgentMove = getTimeDriver(500);
+    const point = getMidPointFromIndex(destination_position);
+
+    agent.transform.x = shiftx(tdAgentMove, agent, point[0]);
+    agent.transform.z = shiftz(tdAgentMove, agent, point[1]);
+    tdAgentMove.start();
+
+    return destination_position;
 }
